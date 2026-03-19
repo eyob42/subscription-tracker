d1ba523f21e5df16bbee55254b96d917c211a32a
eyob42 <eyobadigeh07@gmail.com>
2026-03-19 03:58:16 +0300

    feat(user): Add API endpoints for fetching user data

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req,res) => res.send({title: 'GET all users' }));
-userRouter.get('/:id', (req,res) => res.send({title: 'GET user detail' }));
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser);
 userRouter.post('/', (req,res) => res.send({title: 'CREATE new user' }));
 userRouter.put('/:id', (req,res) => res.send({title: 'UPDATE user' }));
 userRouter.delete('/:id', (req,res) => res.send({title: 'DELETE user' }));
